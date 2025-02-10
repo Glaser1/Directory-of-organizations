87399e872d423782e35e44907ef3ddc00731594e
Alexandr <sashapliskov@gmail.com>
2025-02-10 16:36:13 +0300

    Create db helper

---
+++ fastapi-application/db_helper.py
@@ -0,0 +1,38 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker, AsyncSession
+from config import settings
+
+
+class DataBaseHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        max_overflow: int = 10,
+        pool_size: int = 15,
+    ):
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    async def dispose(self):
+        await self.engine.dispose()
+
+    async def get_db(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DataBaseHelper(
+    str(settings.db.url),
+    echo=settings.db.echo,
+)
